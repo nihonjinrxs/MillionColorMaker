394a686237f0c0054f89eb1ddbb10ab74d20b430
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2015-06-16 22:46:30 -0400

    Initial Commit

---
+++ Million Color MakerTests/Million_Color_MakerTests.swift
@@ -0,0 +1,35 @@
+//
+//  Million_Color_MakerTests.swift
+//  Million Color MakerTests
+//
+//  Created by Ryan Harvey on 6/16/15.
+//  Copyright © 2015 datascientist.guru. All rights reserved.
+//
+
+import XCTest
+
+class Million_Color_MakerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
