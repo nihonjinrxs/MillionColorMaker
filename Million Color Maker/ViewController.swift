e3602778318c9e9b333a557254ae61e8473bf51a
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2015-06-16 23:38:52 -0400

    Initial commit with working functionality.

--- Million Color Maker/ViewController.swift
+++ Million Color Maker/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var sliderContainerView: UIView!
+    @IBOutlet weak var colorDisplayView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        updateColorDisplay(redSlider)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func updateColorDisplay(sender: UISlider) {
+        let r: CGFloat = CGFloat(redSlider.value)
+        let g: CGFloat = CGFloat(greenSlider.value)
+        let b: CGFloat = CGFloat(blueSlider.value)
+        
+        let k: CGFloat = 0.8 - (0.6 * (r + g + b)/3.0)
+        
+        colorDisplayView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+        sliderContainerView.backgroundColor = UIColor(red: k, green: k, blue: k, alpha: 1)
+    }
 
 }
 
